7e8571d33ac62a613f9e161b23df05eb40f14b81
220101ft <220101ft@yse-c.net>
2023-10-11 11:38:27 +0900

    update

--- express_sample/server.js
+++ express_sample/server.js
@@ -40,10 +40,16 @@ app.post('/auth', (req, res) => {
     console.log(loginName, password)
 
     var message = "ログイン失敗"
+    // .envで設定した値でログインチェック
+    // TODO: データベースに接続してユーザ取得
+    // TODO: パスワードはハッシュ値でチェック
     if (loginName == process.env.LOGIN_NAME && password == process.env.PASSWORD) {
         message = "ログイン成功"
+        // TODO: ログインが成功したらユーザの状態を保存
+        // TODO: ログイン後のページの転送
+    } else {
+        // TODO: ログイン画面に戻す
     }
-
     res.send(message)
 })
 
