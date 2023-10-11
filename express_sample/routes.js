a2b14eb8c3d168c207cfc152ca121529fb86e548
220101ft <220101ft@yse-c.net>
2023-10-11 11:53:46 +0900

    add route module

---
+++ express_sample/routes.js
@@ -0,0 +1,42 @@
+// expressモジュール読み込み
+const express = require('express')
+// Routerオブジェクトを生成
+const router = express.Router()
+
+// GETリクエストの処理
+router.get('/', (req, res) => {
+    // リクエストの処理
+    console.log(req.body)
+    console.log(req.url)
+    console.log(req.query)
+
+    // レスポンスの処理
+    res.send('Hello!!')
+})
+
+router.get('/profile', (req, res) => {
+    res.send('プロフィール')
+})
+
+// POSTリクエスト
+router.post('/auth', (req, res) => {
+    var loginName = req.body.login_name
+    var password = req.body.password
+    console.log(loginName, password)
+
+    var message = "ログイン失敗"
+    // .envで設定した値でログインチェック
+    // TODO: データベースに接続してユーザ取得
+    // TODO: パスワードはハッシュ値でチェック
+    if (loginName == process.env.LOGIN_NAME && password == process.env.PASSWORD) {
+        message = "ログイン成功"
+        // TODO: ログインが成功したらユーザの状態を保存
+        // TODO: ログイン後のページの転送
+    } else {
+        // TODO: ログイン画面に戻す
+    }
+    res.send(message)
+})
+
+// モジュール化
+module.exports = router
